8bc1255d4f8c6f3edc80f9989eddf58cf963c5c5
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-04-30 01:11:49 -0400

    SpringBoot intiazlier

---
+++ backend/src/main/java/com/applyzen/applyzen/ApplyZenApplication.java
@@ -0,0 +1,13 @@
+package com.applyzen.applyzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyZenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/applyzen/applyzen/ApplyZenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applyzen.applyzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
