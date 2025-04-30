33ba96bf313f089564858ebc425f3501795cb322
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-04-30 18:05:01 -0400

    Validation for Status, User and JobApplication

--- backend/src/main/java/com/applyzen/applyzen/jobapplication/JobApplication.java
+++ backend/src/main/java/com/applyzen/applyzen/jobapplication/JobApplication.java
@@ -4,6 +4,11 @@ import com.applyzen.applyzen.status.Status;
 import com.applyzen.applyzen.user.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDateTime;
 
@@ -20,11 +25,16 @@ public class JobApplication {
             generator = "job_application_id_generator"
     )
     private Long id;
+    @NotBlank(message = "Cannot be blank")
     private String companyName;
+    @NotBlank(message = "Cannot be blank")
     private String jobTitle;
+    @NotNull(message = "Cannot be null")
     @FutureOrPresent(message = "Must be a future or present date and time")
     private LocalDateTime dateApplied;
+    @URL(message = "Must be a valid URL")
     private String postingURL;
+    @Length(min = 0, max = 255, message = "Must be between 0 and 255 characters long")
     private String notes;
     @FutureOrPresent(message = "Must be a future or present date and time")
     private LocalDateTime createdAt;
--- backend/src/main/java/com/applyzen/applyzen/status/Status.java
+++ backend/src/main/java/com/applyzen/applyzen/status/Status.java
@@ -4,6 +4,7 @@ import com.applyzen.applyzen.jobapplication.JobApplication;
 import com.applyzen.applyzen.user.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,6 +22,7 @@ public class Status {
             generator = "status_id_generator"
     )
     private Long id;
+    @NotBlank(message = "Cannot be blank")
     private String name;
     @FutureOrPresent(message = "Must be a future or present date and time")
     private LocalDateTime createdAt;
